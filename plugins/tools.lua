76591ac7d1c49f20bc00c4aeb2483551665c3158
hsamdew <hsamdew@gmail.com>
2017-04-08 21:24:28 +0430

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 167419742 -- put Your ID here! <===
+local SUDO = 317576202 -- put Your ID here! <===
 local function index_function(user_id)
   for k,v in pairs(_config.admins) do
     if user_id == v[1] then
