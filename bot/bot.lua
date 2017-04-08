46c02eca3432ebcd417e39fee86656f1f2988b52
hsamdew <hsamdew@gmail.com>
2017-04-08 21:41:57 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,7 +1,7 @@
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
 feedparser = (loadfile "./libs/feedparser.lua")()
-our_id = 167419742 -- Put Here Your Bot ID
+our_id = 239465917 -- Put Here Your Bot ID
 --ایدی رباتتونو اینجا بزارید
 URL = require "socket.url"
 http = require "socket.http"
